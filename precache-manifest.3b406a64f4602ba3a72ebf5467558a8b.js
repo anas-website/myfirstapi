dab637a692a24cc8b3ca502dce3d47561f9d0b7e
anas <anas3506606@gmail.com>
2020-06-10 20:11:11 +0300

    Updates

---
+++ precache-manifest.3b406a64f4602ba3a72ebf5467558a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9d01e8bcc3c56b8c57bdd9dac9e975",
+    "url": "/myfirstapi/index.html"
+  },
+  {
+    "revision": "e5ad9894bbee921b0cf3",
+    "url": "/myfirstapi/static/css/2.ccd0549d.chunk.css"
+  },
+  {
+    "revision": "e5ad9894bbee921b0cf3",
+    "url": "/myfirstapi/static/js/2.854129a3.chunk.js"
+  },
+  {
+    "revision": "ee398ec1421b0b11f592",
+    "url": "/myfirstapi/static/js/main.279570de.chunk.js"
+  },
+  {
+    "revision": "43ab4a12adb68f456ae9",
+    "url": "/myfirstapi/static/js/runtime~main.095dd475.js"
+  }
+]);
