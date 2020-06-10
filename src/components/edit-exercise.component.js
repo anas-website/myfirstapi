cbc66a078c0e5fb795d2f45a44f8ebaeb322a11a
anas <anas3506606@gmail.com>
2020-06-10 20:25:40 +0300

    first commit

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -90,7 +90,7 @@ export default class EditExercise extends Component {
     axios.post('https://myapifirs.herokuapp.com/exercises/update/' + this.props.match.params.id, exercise)
       .then(res => console.log(res.data));
 
-    window.location = '/';
+    // window.location = '/';
   }
 
   render() {
